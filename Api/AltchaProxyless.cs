2dd855df246555fc01514d9193827938430ef5b6
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2026-01-19 08:38:27 +0100

    Add Altcha captcha support
    anti-captcha/anticaptcha-csharp@61a808101fd22281d0ab32dc740c27e684bc824a

---
+++ Api/AltchaProxyless.cs
@@ -0,0 +1,27 @@
+using Anticaptcha.ApiResponse;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Anticaptcha.Api
+{
+    public class AltchaProxyless : AnticaptchaBase, IAnticaptchaTaskProtocol
+    {
+        public Uri WebsiteUrl { protected get; set; }
+        public string ChallengeURL { protected get; set; }
+
+        public override JObject GetPostData()
+        {
+            return new JObject
+            {
+                {"type", "AltchaTaskProxyless"},
+                {"websiteURL", WebsiteUrl},
+                {"challengeURL", ChallengeURL}
+            };
+        }
+
+        public TaskResultResponse.SolutionData GetTaskSolution()
+        {
+            return TaskInfo.Solution;
+        }
+    }
+}
