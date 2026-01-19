ad4d9fd7ee93375ccc359b2f0fba6a5ee07b30e0
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2026-01-19 08:32:00 +0100

    Add Prosopo
    anti-captcha/anticaptcha-csharp@8ed756654293acfc642c81b39acf5981536a46a9

---
+++ Api/ProsopoProxyless.cs
@@ -0,0 +1,27 @@
+using Anticaptcha.ApiResponse;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Anticaptcha.Api
+{
+    public class ProsopoProxyless : AnticaptchaBase, IAnticaptchaTaskProtocol
+    {
+        public Uri WebsiteUrl { protected get; set; }
+        public string WebsiteKey { protected get; set; }
+
+        public override JObject GetPostData()
+        {
+            return new JObject
+            {
+                {"type", "ProsopoTaskProxyless"},
+                {"websiteURL", WebsiteUrl},
+                {"websiteKey", WebsiteKey}
+            };
+        }
+
+        public TaskResultResponse.SolutionData GetTaskSolution()
+        {
+            return TaskInfo.Solution;
+        }
+    }
+}
