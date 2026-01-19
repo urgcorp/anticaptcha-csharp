4bf8ef987aea9d6c1517565b598d0adc3d503c55
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2026-01-19 08:29:25 +0100

    Add Friendly Captcha
    anti-captcha/anticaptcha-csharp@8ed756654293acfc642c81b39acf5981536a46a9

---
+++ Api/FriendlyCaptchaProxyless.cs
@@ -0,0 +1,27 @@
+using Anticaptcha.ApiResponse;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Anticaptcha.Api
+{
+    public class FriendlyCaptchaProxyless : AnticaptchaBase, IAnticaptchaTaskProtocol
+    {
+        public Uri WebsiteUrl { protected get; set; }
+        public string WebsiteKey { protected get; set; }
+
+        public override JObject GetPostData()
+        {
+            return new JObject
+            {
+                {"type", "FriendlyCaptchaTaskProxyless"},
+                {"websiteURL", WebsiteUrl},
+                {"websiteKey", WebsiteKey}
+            };
+        }
+
+        public TaskResultResponse.SolutionData GetTaskSolution()
+        {
+            return TaskInfo.Solution;
+        }
+    }
+}
