27e030661df5935580fdcaa3cfc501512dd9dfb8
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2026-01-19 08:02:34 +0100

    Update HCaptcha.cs

--- Api/HCaptcha.cs
+++ Api/HCaptcha.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using Anticaptcha.Helper;
 
 namespace Anticaptcha.Api
@@ -11,28 +10,24 @@ namespace Anticaptcha.Api
         public int? ProxyPort { protected get; set; }
         public string ProxyLogin { protected get; set; }
         public string ProxyPassword { protected get; set; }
-        public string UserAgent { protected get; set; }
         public string Cookies { protected get; set; }
 
         public override JObject GetPostData()
         {
-            var postData = base.GetPostData();
-            postData["type"] = "HCaptchaTask";
-
-            if (ProxyType == null || ProxyPort == null || ProxyPort < 1 || ProxyPort > 65535 ||
-                string.IsNullOrEmpty(ProxyAddress))
+            if (!ProxyHelper.IsValidProxy(ProxyType, ProxyPort, ProxyAddress))
             {
                 DebugHelper.Out("Proxy data is incorrect!", DebugHelper.Type.Error);
-
                 return null;
             }
+
+            var postData = base.GetPostData();
+            postData["type"] = "HCaptchaTask";
 
             postData.Add("proxyType", ProxyType.ToString().ToLower());
             postData.Add("proxyAddress", ProxyAddress);
             postData.Add("proxyPort", ProxyPort);
             postData.Add("proxyLogin", ProxyLogin);
             postData.Add("proxyPassword", ProxyPassword);
-            postData.Add("userAgent", UserAgent);
             postData.Add("cookies", Cookies);
 
             return postData;
